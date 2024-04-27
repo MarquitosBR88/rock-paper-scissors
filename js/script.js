768e247ea3601cfc7f9f924e42c5f4b23d52b9b5
Marcus <marcus.vi.mar@gmail.com>
2024-04-27 20:23:35 -0300

    Create function getHumanChoice()

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3)
-    if (choice === 0) {
+    let computerChoice = Math.floor(Math.random() * 3)
+    if (computerChoice === 0) {
         return "rock";
-    } else if (choice === 1) {
+    } else if (computerChoice === 1) {
         return "paper";
     } else {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+    if (humanChoice === "rock") {
+        return "rock";
+    } else if (humanChoice === "paper") {
+        return "paper";
+    } else if (humanChoice === "scissors") {
+        return "scissors";
+    } else if (humanChoice === null) {
+        alert("Canceled")
+    } else {
+        alert("This isn't a valid option.")
+    }
+}
+
