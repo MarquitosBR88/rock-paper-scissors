bfdb751948293b46e11c512c01d60baee826922d
Marcus <marcus.vi.mar@gmail.com>
2024-04-28 12:12:11 -0300

    Create function playRound

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 
+let humanScore = 0 
+let computerScore = 0
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3)
     if (computerChoice === 0) {
@@ -12,16 +21,31 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper or Scissors?");
-    if (humanChoice === "rock") {
+    if (humanChoice.toLowerCase() === "rock") {
         return "rock";
-    } else if (humanChoice === "paper") {
+    } else if (humanChoice.toLowerCase() === "paper") {
         return "paper";
-    } else if (humanChoice === "scissors") {
+    } else if (humanChoice.toLowerCase() === "scissors") {
         return "scissors";
     } else if (humanChoice === null) {
-        alert("Canceled")
+        alert("Canceled");
     } else {
-        alert("This isn't a valid option.")
+        alert("This isn't a valid option.");
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "rock") || (humanChoice === "paper" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "scissors")) {
+        console.log("Draw, both chose the same option.");
+    } else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else {
+        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+        computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+}
